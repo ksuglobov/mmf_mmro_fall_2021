ded616f8ba13118a7bb6bcd2dcde2a98f1e92b06
ksuglobov <82650002+ksuglobov@users.noreply.github.com>
2021-10-18 07:50:55 +0300

    Example and completed task 1

---
+++ Example/example.py
@@ -0,0 +1,16 @@
+from colorama import init, Fore, Back, Style
+
+init()
+
+def console_picture():
+    print(Style.BRIGHT + Fore.YELLOW)
+    print("                **    **  ********  **        **            **      ")
+    print("               **    **  ********  **        **         **     **   ")
+    print("              ********  **        **        **         **      **  ")
+    print("             ********  ********  **        **         **      **  ")
+    print("            **    **  **        **        **         **      **  ")
+    print("           **    **  ********  ********  ********    **    **   ")
+    print("          **    **  ********  ********  ********       **      ")
+
+console_picture()
+input()
